c2a6aa5160750fca4295a45070c73d53a6eb5232
Michael Tung <Michael Tung@MICHAEL-N56>
2018-04-17 09:35:47 +0800

    添加项目文件。

---
+++ AngularJS_L001/personController.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('myApp', [])
+
+app.controller('namesCtrl', function ($scope) {
+    $scope.names = [
+        { name: 'Jani', country: 'Norway' },
+        { name: 'Hege', country: 'Sweden' },
+        { name: 'Kai', country: 'Denmark' }
+    ];
+});
+
+app.controller('personCtrl', function ($scope) {
+    $scope.firstName = "John",
+        $scope.lastName = "Doe",
+        $scope.fullName = function () {
+            return $scope.firstName + " " + $scope.lastName;
+        }
+});
+app.controller('costCtrl', function ($scope) {
+    $scope.quantity = 1;
+    $scope.price = 9.99;
+});
